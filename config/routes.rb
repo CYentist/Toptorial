1360b55accf125bfe039508c5cabd7d4e9b2fac7
seewhymoon <seewhymoon@gmail.com>
2017-05-11 21:38:11 +0800

    tutorial crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-  root 'welcome#index'
+  devise_for :users
+  resources :tutorials
+  root 'tutorials#index'
 
 end
