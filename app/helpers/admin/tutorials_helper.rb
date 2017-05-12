ceedb66baa809de9a362789d47e265cb3e7b2154
CYentist <seewhymoon@gmail.com>
2017-05-12 15:23:54 +0800

    Revert "add admin check function"

--- app/helpers/admin/tutorials_helper.rb
+++ app/helpers/admin/tutorials_helper.rb
@@ -1,9 +1,2 @@
 module Admin::TutorialsHelper
-  def render_tutorial_status(tutorial)
-    if tutorial.checked
-      "(已上架)"
-    else
-      "(待审核)"
-    end
-  end
 end
--- db/migrate/20170512022745_add_checked_to_tutorial.rb
+++
@@ -1,5 +0,0 @@
-class AddCheckedToTutorial < ActiveRecord::Migration[5.0]
-  def change
-    add_column :tutorials, :checked, :boolean, default: false
-  end
-end
