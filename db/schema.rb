c3cac35a1a92b6dd8c68a9dc5b577dd43676c01b
seewhymoon <seewhymoon@gmail.com>
2017-05-13 21:17:37 +0800

    add description to tutorial

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170512022745) do
+ActiveRecord::Schema.define(version: 20170513130132) do
 
   create_table "tutorials", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.integer  "user_id"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.boolean  "checked",    default: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.boolean  "checked",     default: false
+    t.text     "description"
   end
 
   create_table "users", force: :cascade do |t|
