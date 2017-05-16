626e4383e1f0414bcf4f7f0d029742f83b39942a
whispeed <whispeed@163.com>
2017-05-16 20:56:23 +0800

    add Gem annotate

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id          :integer          not null, primary key
+#  tutorial_id :integer
+#  user_id     :integer
+#  post        :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Comment < ApplicationRecord
   belongs_to :tutorial
   belongs_to :user
--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: tutorials
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  content     :text
+#  user_id     :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  checked     :boolean          default("f")
+#  description :text
+#  image       :string
+#  price       :integer
+#
+
 class Tutorial < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :user
