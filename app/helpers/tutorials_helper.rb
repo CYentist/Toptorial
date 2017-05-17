6c81b07922f0f357b61c9f9f03779078efdb81a9
seewhymoon <seewhymoon@gmail.com>
2017-05-17 16:31:02 +0800

    index pic and others

--- app/helpers/tutorials_helper.rb
+++ app/helpers/tutorials_helper.rb
@@ -1,2 +1,10 @@
 module TutorialsHelper
+  def render_tutorial_image
+    if @tutorial.image.present?
+      @tutorial.image.thumb.url
+    else
+      "http://placehold.it/200x200&text=No Pic"
+    end
+  end
+
 end
